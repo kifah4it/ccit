8e11473662f0b818e17ffd9fc9b7ace6d8bb5806
kifah4it <kifah4it@outlook.com>
2024-02-14 10:11:53 +0200

    Merge Courses v1

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -3,20 +3,181 @@
 Courses
 @endsection
 @section('content')
-<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/courses_top.jpg');">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
+          <!--<div class="col-md-9 ftco-animate text-center">
             <h1 class="mb-2 bread">Courses</h1>
             <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Courses <i class="ion-ios-arrow-forward"></i></span></p>
-          </div>
+          </div> !--->
         </div>
       </div>
     </section>
 
+
+     <section class="container-fluid Languages">
+    
+    <div class=" row English_top ">
+		
+	<br>
+	<img class="First_icon" src="images/E_icon.PNG" > 
+	<span><b>nglish Courses</b></span>
+		 
+    <img class="InterChange_image" src="images/InterChange.PNG" >
+	
+
+</div>
+
+<div class="container-fluid">
+
+    <div class="row English" >
+   
+<div class="col-md-4">
+	<div class="Course">
+<span style=color:#fecd29><b>Ventures</b></span>
+ <h5>Foundation</h5>
+<img class="Course_Im" src="images/Ve_Im.JPEG" >
+
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#e9d129><b>Intro A</b></span>
+<h5>Foundation</h5>
+<img class="Course_Im" src="images/Intro.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#e9d129><b>Intro B</b></span>
+<h5>Beginner</h5>
+<img class="Course_Im" src="images/Intro.JPG" >
+
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#e96c00><b>Let's Talk 1</b></span>
+<h5>Beginner</h5>
+<img class="Course_Im" src="images/Talk1.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#d52316><b>1A</b></span > 
+<h5>Beginner</h5>
+<img class="Course_Im" src="images/L1.JPG">
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#d52316><b>1B</b></span>
+<h5>Beginner</h5>
+<img class="Course_Im" src="images/L1.JPG">
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#67b142><b>Let's Talk 2</b></span>
+<h5>Pre-intermediate</h5>
+<img class="Course_Im" src="images/Talk2.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#2c59c3><b>2A</b></span>
+<h5>Pre-intermediate</h5>
+<img class="Course_Im" src="images/L2.JPG">
+</div>  
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#2c59c3><b>2B</b></span>
+<h5>Intermediate</h5>
+<img class="Course_Im" src="images/L2.JPG">
+</div>
+</div> 
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#96579d><b>Let's Talk 3</b></span>
+<h5>Intermediate</h5>
+<img class="Course_Im" src="images/Talk3.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#10964d><b>3A</b></span>
+<h5>Intermediate</h5>
+<img class="Course_Im" src="images/L3.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#10964d><b>3B</b></span>
+<h5>Upper-intermediate</h5>
+<img class="Course_Im" src="images/L3.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#e54327><b>4A</b></span>
+<h5>Upper-intermediate</h5>
+<img class="Course_Im" src="images/L4.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#e54327><b>4B</b></span>
+<h5>Upper-intermediate</h5>
+<img class="Course_Im" src="images/L4.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#2075b3><b>5A</b></span>
+<h5>Advanced</h5>
+<img class="Course_Im" src="images/L5.JPG" >
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="Course">
+<span style=color:#2075b3><b>5B</b></span>
+<h5>Advanced</h5>
+<img class="Course_Im" src="images/L5.JPG" >
+</div>
+</div>
+</div>
+</div> 
+
+<div class="row Doutch_top"> 
+<br>
+<img class="row First_icon" src="images/D_icon.png" > 
+	<span><b>outch Courses</b></span>
+		 <br><br>    
+</div> 
+
+</section>
+
     <section class="ftco-section">
+	<div class="options">
 			<div class="container-fluid px-4">
+
 				<!-- <div class="row">
 				<?php /*	@foreach ($courseslst as $course)
 					@if(!isset($course->Name))
@@ -54,6 +215,7 @@ Courses
 					</div>
 				</div>
 			</div>
+</div>
 		</section>
 @endsection
 @section('scripts')
