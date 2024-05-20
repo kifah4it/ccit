b3452d65c6fbd7699330cf386a77414ce9bdcef7
kifah4it <kifah4it@outlook.com>
2024-05-20 16:40:21 +0300

    update home controller: redirections, caching, course availability, add curriculum action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,17 +9,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 class HomeController extends Controller
 {
     //
     public function index(){
-        if(isset($_GET['sesskey'])){
-            if(!isset($_SESSION))
+        if(!isset($_SESSION))
                 session_start();
-
-            $_SESSION['mdl_sesskey'] = $_GET['sesskey'];
-            return Redirect::to(env('LMS_URL')."/my");
+        
+        if(isset($_GET['sesskey'])){
+        $_SESSION['mdl_sesskey'] = $_GET['sesskey'];
+        
+        if(isset($_SESSION['redirect'])){
+            $redir = $_SESSION['redirect'];
+            unset($_SESSION['redirect']);
+            return Redirect::to($redir);
+            }
+            else
+                return Redirect::to(env('LMS_URL')."/my");
         }
+
+        
         // require_once('geoplugin.class.php');
         // $geoplugin = new geoPlugin();
 // If we wanted to change the base currency, we would uncomment the following line
@@ -46,45 +56,43 @@ class HomeController extends Controller
 // 	"Exchange Rate: {$geoplugin->currencyConverter} <br />\n";
         return view('home');
     }
+
     public function courses($cat = null){
-        $LMS_URL = env('LMS_URL');
-         $url = $LMS_URL.'/webservice/rest/server.php?wstoken=505320c31891faef39a5c0c900220763&wsfunction=local_ops_get_courses_with_parent_cat&catname='.$cat.'&moodlewsrestformat=json';
-         //$url = $LMS_URL.'/webservice/rest/server.php?wstoken=505320c31891faef39a5c0c900220763&wsfunction=local_ops_enroll_student&username=testsss&courses[0]=13&moodlewsrestformat=json';
-      //  $url = 'http://localhost:8080/ccit/resources/Courses.json';
-        // $response = HTTP::withOptions([
-            
-        // ])->get($url);
-        $ch = curl_init();
-     $headers = array(
-        'Accept: application/json',
-        'Content-Type: application/json',
-        );
+    //     if(!Cache::has('courses_cache')){
+    //     $LMS_URL = env('LMS_URL');
+    //      $url = $LMS_URL.'/webservice/rest/server.php?wstoken=505320c31891faef39a5c0c900220763&wsfunction=local_ops_get_courses_with_parent_cat&catname='.$cat.'&moodlewsrestformat=json';
         
-     curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
-     // Timeout in seconds
-     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    //     $ch = curl_init();
+    //  $headers = array(
+    //     'Accept: application/json',
+    //     'Content-Type: application/json',
+    //     );
+        
+    //  curl_setopt($ch, CURLOPT_URL,$url);
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+    //     curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+    //  // Timeout in seconds
+    //  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
  
-      $response = curl_exec($ch);
-        $courses = json_decode($response,true);
-       // var_dump($courses['courses']);
+    //   $response = curl_exec($ch);
+    //     $courses = json_decode($response,true);
+        require_once('CacheManagement.php');
+        $cache = new CacheManagement();
+        $courses = $cache->getcache();
         $courseslst[] = array();
         
-        foreach($courses['courses'] as $course){
+        foreach($courses as $course){
            $CourseV = new CoursesView();
             $CourseV->Name = $course['fullname'];
             // $CourseV->Name = $course['customfields'][0]['value'] ?? '';
-            $CourseV->imgSrc = $course['courseimage'];
+            $CourseV->imgSrc = env('LMS_URL').'/'.(string) $course['courseimage'];
             $CourseV->sDesc = strip_tags($course['summary']);
-            $CourseV->catName = $course['categoryname'];
+            $CourseV->catName = (string) $course['categoryname'];
             $courseslst[] = $CourseV;
         }
-       // var_dump($courseslst);
       return view('courses')->with('courseslst',$courseslst);
-    return view('courses');
       
 }
 
@@ -103,13 +111,84 @@ class HomeController extends Controller
             return redirect()->back()->withCookie(cookie()->forever('lang','EN'));
         }
     }
-    public function course(){
-        return view('course');
+    private function checkcourseavailabilty($username,$cid){
+        $LMS_URL = env('LMS_URL');
+        $key = env('localops_API_key');
+        $url = $LMS_URL.'/webservice/rest/server.php?wstoken='.$key.'&wsfunction=local_ops_check_course_prereq&username='.$username.'&coursesids[]='.$cid.'&moodlewsrestformat=json';
+       $ch = curl_init();
+    $headers = array(
+       'Accept: application/json',
+       'Content-Type: application/json',
+       );
+       
+    curl_setopt($ch, CURLOPT_URL,$url);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+       curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+    // Timeout in seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+     $response = curl_exec($ch);
+       $courses = json_decode($response,true);
+       return $courses;
+    }
+    public function course($name){
+        require_once('CacheManagement.php');
+        $cache = new CacheManagement();
+        $courses = $cache->getcache();
+        if(!isset($_SESSION))
+        session_start();
+        foreach($courses as $c){
+            if($c['fullname'] == $name){
+                if(isset($_SESSION['mdl_userinfo'])){
+                   //$avail = self::checkcourseavailabilty($_SESSION['mdl_userinfo']->username,$c['id']);
+                    
+                   $c = array_merge((array)$c,array('avail'=>self::checkcourseavailabilty($_SESSION['mdl_userinfo']->username,$c['id']),true));
+                }
+                return view('course')->with('courseObj',$c);
+            }
+        }
+      //  return view('course');
+    }
+    public function curriculum($name){
+        require_once('CacheManagement.php');
+        $cache = new CacheManagement();
+        $courses = $cache->getcache();
+        $crclm = array();
+        if(!isset($_SESSION))
+        session_start();
+        foreach($courses as $c){
+            if($c['fullname'] == $name){
+                if(isset($_SESSION['mdl_userinfo']))
+                $c = array_merge((array)$c,array('avail'=>self::checkcourseavailabilty($_SESSION['mdl_userinfo']->username,$c['id']),true));
+
+                $crclm[] = $c;
+                
+                foreach($courses as $cr){
+                    if($c['categoryid'] == $cr['categoryid'] && $cr['format'] != "singleactivity"){
+
+                        if(isset($_SESSION['mdl_userinfo']))
+                        $cr = array_merge((array)$cr,array('avail'=>self::checkcourseavailabilty($_SESSION['mdl_userinfo']->username,$cr['id']),true));
+
+                        $crclm[0]['courses'][] = $cr;
+                    }
+                }
+                return view('curriculum')->with('courseObj',$crclm);
+            }
+                
+        }
+      //  return view('course');
     }
     public function login(){
-        if($_GET['action'] == 'logout'){
+        if(isset($_GET['action']) && $_GET['action'] == 'logout'){
             unset($_SESSION['mdl_userinfo']);
             return view('home');
+        }
+        if(isset($_GET['redirect'])){
+            if(!isset($_SESSION))
+                session_start();
+                $_SESSION['redirect'] = $_GET['redirect'];
         }
         return view('login');
     }
