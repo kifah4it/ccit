18e5d410a8f8c6ebc271163ff4054c7b40ebf763
kifah4it <kifahalhoumaide@gmail.com>
2024-06-05 15:27:20 +0300

    handle null values of sDesc in Courses controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,7 +79,7 @@ class HomeController extends Controller
             $CourseV->Name = $course['fullname'];
             // $CourseV->Name = $course['customfields'][0]['value'] ?? '';
             $CourseV->imgSrc = env('LMS_URL').'/'.(string) $course['courseimage'];
-            $CourseV->sDesc = Cookie::get('lang') == 'EN' ? $course['course_details_en'] : $course['course_details'];
+            $CourseV->sDesc = Cookie::get('lang') == 'EN' ? $course['course_details_en'] ?? '' : $course['course_details'] ?? '';
             $CourseV->catName = (string) $course['categoryname'];
             $courseslst[] = $CourseV;
         }
